80cc53387533da85cc62cd339f504f7dc32f2f34
agustin purici <agustinmp.2002@gmail.com>
2026-07-22 13:12:39 -0300

    notranslate

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { DomErrorBoundary } from "./Domerrorboundary";
 import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <DomErrorBoundary>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-    </DomErrorBoundary>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
+
